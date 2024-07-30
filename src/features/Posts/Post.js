9a2cf5edb1bcaee61f1dd4e7aa9f81c759dc7609
Hamdan_Vohra <hamdanvohra5676@gmail.com>
2024-07-30 21:15:57 +0500

    This is update by using axios and async thunk

--- src/features/Posts/Post.js
+++ src/features/Posts/Post.js
@@ -1,29 +1,36 @@
-import {useSelector} from 'react-redux'
-import { selectAllPosts } from './postSlice';
-import PostAuthor from './PostAuthor';
-import TimeAgo from './TimeAgo';
-import ReactionButtons from './ReactionButtons';
+import {useDispatch, useSelector} from 'react-redux'
+import { getAllPosts,getPostsStatus,getPostsError,fetchPosts } from './postSlice';
+import { useEffect } from 'react';
+import EachPost from './EachPost';
+
 
 const Post = () => {
-    const posts = useSelector(selectAllPosts);
-    const orderedPosts = posts.slice().sort((a,b)=>b.date.localeCompare(a.date));
-    const renderPosts = orderedPosts.map((post) => {
-      return(
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0,100)}</p>
-            <p className='postCredit'>
-              <PostAuthor userId={post.userId}/>
-              <TimeAgo timestamp={post.date}/>
-            </p>
-            <ReactionButtons post={post}/>
-        </article>
-      )
-  });
+    const dispatch = useDispatch();
+
+    const posts = useSelector(getAllPosts);
+    // const postStatus = useSelector(getPostsStatus);
+    // const postError = useSelector(getPostsError);
+
+    // useEffect(()=>{
+    //   if(postStatus === 'idle'){
+    //     dispatch(fetchPosts);
+    //   }
+    // },[postStatus,dispatch])
+
+    let content;
+    // if (postStatus === 'loading') {
+    //     content = <p>"Loading..."</p>;
+    // } else if (postStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+        content = orderedPosts.map(post => <EachPost key={post.id} post={post} />)
+    // } else if (postStatus === 'failed') {
+    //     content = <p>{postError}</p>;
+    // }
+
   return (
     <section>
         <h2>Posts</h2>
-        {renderPosts}
+        {content}
     </section>
   )
 }
--- src/features/Posts/postSlice.js
+++ src/features/Posts/postSlice.js
@@ -1,32 +1,28 @@
-import {createSlice,nanoid} from '@reduxjs/toolkit'
+import {createSlice,nanoid,createAsyncThunk} from '@reduxjs/toolkit'
 import {sub} from 'date-fns'
+import axios from 'axios'
 
-const initialState = [
-    {
-        id:'1',
-        title:'Redux Journey',
-        content:'This is my react-redux learning',
-        date:sub(new Date(),{minutes:10}).toISOString(),
-        reactions:{
-            thumbsUp:0,
-            wow:0,
-            heart:0,
-            rocket:0,
-        }
-    },
-    {
-        id:'2',
-        title:'Immer js',
-        content:'This provides advantage to mutate an immutable js data structures using draft.So we can directly push into the states',
-        date:sub(new Date(),{minutes:5}).toISOString(),
-        reactions:{
-            thumbsUp:0,
-            wow:0,
-            heart:0,
-            rocket:0,
-        }
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+const initialState = {
+    posts:[],
+    status:'idle',
+    error:null,
+}
+
+//creating async thunk function that fetches data from server
+export const fetchPosts = createAsyncThunk('posts/fetchPosts',async()=>{
+    try{
+        const response = await axios.get(POSTS_URL);
+        console.log(response);
+        return response.data;
+    } catch(err){
+        console.log(err.message)
+        return err.message;
     }
-]
+})
+
+
 export const postSlice = createSlice({
     name: 'posts',
     initialState,
@@ -34,7 +30,7 @@ export const postSlice = createSlice({
         postAdded:{
             reducer(state,action){
                 //this is only possible due to immerjs as redux toolkit support it
-                state.push(action.payload);
+                state.posts.push(action.payload);
             },
             prepare(title,content,userId){
                 return{
@@ -56,16 +52,48 @@ export const postSlice = createSlice({
         },
         reactionAdded:(state,action)=>{
             const {postId,reaction} = action.payload;
-            const existingPost = state.find(post => post.id === postId);
+            const existingPost = state.posts.find(post => post.id === postId);
             if(existingPost){
                 existingPost.reactions[reaction]++
             }
         }
+    },
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchPosts.pending,(state,action)=>{
+            state.status = 'loading';
+        })
+        .addCase(fetchPosts.fulfilled,(state,action)=>{
+            state.status = 'succeeded'
+
+            //Adding date and reactions
+            let addMin = 1;
+            const loadedPosts = action.payload.map(post=>{
+                //we can add data as immer js make sure it is not mutation
+                post.date = sub(new Date(),{minute:addMin++}.toISOString());
+                post.reactions = {
+                        thumbsUp:0,
+                        wow:0,
+                        heart:0,
+                        rocket:0,
+                }
+                return post;
+            });
+            console.log(loadedPosts)
+            //Add any fetched posts to the array
+            state.posts = state.posts.concat(loadedPosts);
+        })
+        .addCase(fetchPosts.rejected,(state,action)=>{
+            state.status = 'failed';
+            state.error = action.err.message;
+        })
     }
 }
 )
 
-export const selectAllPosts = (state) => state.posts;
+export const getAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
 
 export const {postAdded,reactionAdded} = postSlice.actions;
 
